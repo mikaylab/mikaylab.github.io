d5ceb2b9b59fab8b859e46d67a38a4c35dc78d64
mikaylab <mbuford@wisc.edu>
2023-02-16 15:26:27 -0600

    Removed excess files. Added p5.js files and scripts

---
+++ js/sketch.js
@@ -0,0 +1,132 @@
+let limeImg;
+let periImg;
+let grapeImg;
+
+function preload() {
+  periImg = loadImage("/images/asset/Periwinkle.png");
+
+  limeImg = loadImage("/images/asset/Lime Green.png");
+
+  grapeImg = loadImage("/images/asset/GrapeTopRight.png");
+}
+
+function setup() {
+  createCanvas(1080, 860);
+}
+
+function draw() {
+  background("#180B3D");
+
+  // GRAPE - Top Left
+  push();
+  // translate to where you want the center of the ellipse to be
+  translate(20, 50);
+  // rotate using the frameCount (increases by one on each frame)
+  rotate(frameCount * 0.01);
+  // place the image at the origin
+  imageMode(CENTER);
+  image(grapeImg, 0, 0, 400, 600);
+  pop();
+
+  // GRAPE - Top Right
+  push();
+  translate(700, 100);
+  rotate(frameCount * 0.01);
+  imageMode(CENTER);
+  image(grapeImg, 0, 0, 500, 500);
+  pop();
+
+  // GRAPE - Center
+  push();
+  translate(460, 250);
+  rotate(frameCount * 0.01);
+  imageMode(CENTER);
+  image(grapeImg, 0, 0, 600, 350);
+  pop();
+
+  // GRAPE - Bottom Right
+  push();
+  translate(700, 500);
+  rotate(frameCount * 0.01);
+  imageMode(CENTER);
+  image(grapeImg, 0, 0, 600, 900);
+  pop();
+
+  // GRAPE - Bottom Left
+  push();
+  translate(70, 510);
+  rotate(frameCount * 0.01);
+  imageMode(CENTER);
+  image(grapeImg, 0, 0, 800, 600);
+  pop();
+
+  // PERIWINKLE - Top Center
+  push();
+
+  translate(450, 80);
+  rotate(frameCount * 0.01);
+  imageMode(CENTER);
+  image(periImg, 0, 0, 500, 500);
+
+  pop();
+  
+  // PERIWINKLE - Left Center
+  push();
+
+  translate(40, 300);
+  rotate(frameCount * 0.01);
+  imageMode(CENTER);
+  image(periImg, 0, 0, 400, 500);
+
+  pop();
+
+  // PERIWINKLE - Bottom Right
+  push();
+
+  translate(650, 400);
+  rotate(frameCount * 0.01);
+  imageMode(CENTER);
+  image(periImg, 0, 0, 200, 200);
+
+  pop();
+  
+  // LIME - Top Left
+  push();
+  translate(200, 100);
+  rotate(frameCount * 0.01);
+  imageMode(CENTER);
+  image(limeImg, 0, 0, 100, 100);
+  pop();
+
+  // LIME - Center Left
+  push();
+  translate(800, 300);
+  rotate(frameCount * 0.01);
+  imageMode(CENTER);
+  image(limeImg, 0, 0, 450, 500);
+  pop();
+
+  // LIME - Center
+  push();
+  translate(300, 300);
+  rotate(frameCount * 0.01);
+  imageMode(CENTER);
+  image(limeImg, 0, 0, 300, 340);
+  pop();
+
+  // LIME - Bottom Right
+  push();
+  translate(350, 600);
+  rotate(frameCount * 0.01);
+  imageMode(CENTER);
+  image(limeImg, 0, 0, 600, 600);
+  pop();
+
+  // LIME - Bottom Left
+  push();
+  translate(200, 100);
+  rotate(frameCount * 0.01);
+  imageMode(CENTER);
+  image(limeImg, 0, 0, 200, 200);
+  pop();
+}
